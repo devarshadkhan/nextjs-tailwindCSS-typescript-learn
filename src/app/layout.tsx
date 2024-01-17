b3f29bc9708dc43bd1acf3b48893ed87ce8235f2
codewitharshad <developer.codewitharsh@gmail.com>
2024-01-18 02:13:21 +0530

    font family add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="font-roboto">
         <div className="container mx-auto">
           <Navbar />
           {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        roboto: ['"Roboto", "sans-serif"']
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
 export default config
